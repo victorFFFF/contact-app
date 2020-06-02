eac6fbc3c3ee25f07f07346d2f78524560d0e438
Victor Li <victor.li.1995@hotmail.com>
2020-06-02 16:06:21 -0400

    finished contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ContactCard from "./ContactCard";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <ContactCard
+          name="John"
+          mobile="2125555555"
+          work="8002222222"
+          email="hello@example.com"
+        />
+        <ContactCard
+          name="Jill"
+          mobile="7185555555"
+          work="7182222222"
+          email="jill@example.org"
+        />
+        <ContactCard
+          name="Mike"
+          mobile="3475555555"
+          work="5552222222"
+          email="mike@example.com"
+        />
+      </>
+    );
+  }
 }
 
 export default App;
